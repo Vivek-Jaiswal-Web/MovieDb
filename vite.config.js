a3a16b1ea3cde63f6df263bac801ccd13773b870
Vivek Jaiswal <vivekrkjaiswal@gmail.com>
2025-02-09 15:55:43 +0000

    changes for the sandbox view port

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: ["md33ng-5173.csb.app"], // Allow the host
+    host: "0.0.0.0", // Allow external access
+    port: 5173, // Set the port
+  },
+});
